879132775f84441deeed40bb6dea92d8ed9edc64
DanIsDaMan <danyaal45@gmail.com>
2018-11-08 21:12:44 +0000

    Sorted out an issue where it would throw an error after installing the first package. Sorted out an issue where it wasn't uploading hashsums correctly.

---
+++ CyUploader.py
@@ -0,0 +1,97 @@
+from shutil import copyfile
+import time
+import sys
+import os
+
+#NOTE : Put .deb files to be added in a folder called "adddebs",
+#and make another folder called "debs". This one will be the desination.
+
+def printLetterByLetter(string):
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+#Get the path of the repo folder.
+#This path is my specific path.
+#path = str("C:\\Users\\hamid\\OneDrive\\Desktop\\DanIsDaMan.github.io\\adddebs")
+#This path will work for anyone as long as this program is in the root of the repo folder.
+path = str(os.getcwd() + "\\adddebs")
+#For every file in the adddebs folder, i.e. Every new .deb file.
+for file in os.listdir(path):
+    file = str(file)
+    #Slices off the ".deb" from the end of the file names.
+    #Needed for the name that shows up on the repo.
+    tweakName = str(file[0:-4])
+    package = str("Package: com.dan." + tweakName)
+    version = str(input("Version Number for " + "\"" + str(tweakName) + "\":\n"))
+    version = str("Version: " + version)
+    #size = str((float(((os.path.getsize(os.getcwd() + "\\adddebs\\" + str(fileName)))/1000))) + "kb")
+    size = float(os.path.getsize(os.getcwd() + "\\adddebs\\" + str(file)))
+    size = float(size/1000)
+    size = str("Size: " + str(size) + "kb")
+    architecture = str("Architecture: iphoneos-arm")
+    description = str(input("Description for " + "\"" + str(tweakName) + "\":\n"))
+    description = str("Description: " + description)
+    maintainerName = str(input("Name of Maintainer:\n"))
+    maintainerEmail = str(input("Maintainer's Email Address:\n"))
+    maintainer = str("Maintainer: " + maintainerName + " <" + maintainerEmail + ">")
+    authorName = str(input("Name of Author:\n"))
+    authorEmail = str(input("Author's Email Address:\n"))
+    author = str("Author: " + authorName + " <" + authorEmail + ">")
+    section = str(input("Section:\n"))
+    section = str("Section: " + section)
+    MD5sum = str(input("MD5sum:\n"))
+    MD5sum = str("MD5sum: " + MD5sum)
+    SHA1sum = str(input("SHA1sum:\n"))
+    SHA1sum = str("SHA1sum: " + SHA1sum)
+    SHA256sum = str(input("SHA256sum:\n"))
+    SHA256sum = str("SHA256sum: " + SHA256sum)
+    fileName = str("debs//" + file)
+    fileName = str("Filename: " + file)
+    filler = str(" ")
+    #Rename "Packages" to "Packages.txt" so that Python can edit it.
+    #os.rename((os.getcwd() + "\\Packages"), (os.getcwd() + "\\Packages.txt"))
+    with open("Packages", "a") as Packages:
+        Packages.write(filler + "\n")
+        Packages.write(package + "\n")
+        Packages.write(version + "\n")
+        Packages.write(size + "\n")
+        Packages.write(architecture + "\n")
+        Packages.write(description + "\n")
+        Packages.write(maintainer + "\n")
+        Packages.write(author + "\n")
+        Packages.write(section + "\n")
+        Packages.write(MD5sum + "\n")
+        Packages.write(SHA1sum + "\n")
+        Packages.write(SHA256sum + "\n")
+        Packages.write(fileName + "\n")
+        Packages.close()
+    #os.rename((os.getcwd() + "\\Packages.txt"), (os.getcwd() + "\\Packages"))
+    filePathOld = str(os.getcwd() + "\\adddebs\\" + file)
+    filePathNew = str(os.getcwd() + "\\debs\\" + file)
+    copyfile(filePathOld, filePathNew)
+    os.remove(filePathOld)
+    success = str("Successfully added " + file + " to your cydia repo!\n")
+    printLetterByLetter(success)
+    
+
+
+
+'''
+#This is the base for the stuff that will be written to the "Packages" file
+Package: com.dan.SpringThing
+Name: SpringThing 
+Version: 4.0.4
+Size: 63kb
+Architecture: iphoneos-arm
+Description: A dope respring animation. Trust me. U want this.
+Maintainer: Danyaal <danyaal45@gmail.com>
+Author: Danyaal <danyaal45@gmail.com>
+Section: Test
+MD5sum:111111111111111111111111
+SHA1sum:11111111111111111111111
+SHA256sum:111111111111111111111
+Filename: debs//SpringThing.deb
+'''
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+This is my program to automatically upload tweaks and stuff to a github cydia repo!
+
+I'm pretty sure that it can run without python as a dependency but idk.
+
+It requires a specific layout of folders & files.
+
+GITHUB PAGES REPO
+
+-adddebs
+	-Add .deb files to upload here
+-debs
+	-Added .deb files go here
+-js,images,assets,css, ect. all other webpage attributes
+
+-unpack bzip2 here if u dont already have it(not in a folder, all bzip2 
+files go directly into the root og the github pages repo folder)
+
+-CyUploader.exe
+-index.html
+-Packages(no file extention)
+-Release(no file extention)
+
+now just run CyUploader :)
