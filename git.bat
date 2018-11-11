b3781ff3aa93aea6d6543c56ef708dbcadb07025
DanIsDaMan <danyaal45@gmail.com>
2018-11-11 20:28:29 +0000

    Test commit using message promt.

--- git.bat
+++ git.bat
@@ -1,5 +1,5 @@
 @echo off
 set /P commitMessage= Enter your commit message here:
 call git.exe add --all
-call git.exe commit -m commitMessage
+call git.exe commit -m "%commitMessage%"
 call git.exe push
